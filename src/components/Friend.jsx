06300d940d28e7eed6e3b1e4b31c19d9498bb25a
Olga Kulczycka <olga@webjaksklep.eu>
2025-10-12 21:41:14 +0300

    friend & friendList components

---
+++ src/components/Friend.jsx
@@ -0,0 +1,13 @@
+export default function Friend({friend}) {
+  return (
+    <li key={friend.id}>
+      <img src={friend.image} alt={friend.name}/>
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && <p className="red">You owe {friend.name} {Math.abs(friend.balance)}$</p>}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      {friend.balance > 0 && <p className="green">{friend.name} owes you {friend.balance}$</p>}
+
+    </li>
+  )
+}
